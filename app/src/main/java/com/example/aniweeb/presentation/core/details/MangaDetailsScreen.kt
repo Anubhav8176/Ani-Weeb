394b17d5fe3a5a3161c9a09e8472b0ed1ade4a19
Anubhav Singh <86763919+Anubhav8176@users.noreply.github.com>
2025-02-18 11:06:37 +0530

    manga detail screen

--- app/src/main/java/com/example/aniweeb/presentation/core/details/MangaDetailsScreen.kt
+++ app/src/main/java/com/example/aniweeb/presentation/core/details/MangaDetailsScreen.kt
@@ -2,6 +2,8 @@ package com.example.aniweeb.presentation.core.details
 
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -13,9 +15,19 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Article
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.LibraryBooks
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material.icons.filled.TrendingUp
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -27,6 +39,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil3.compose.AsyncImage
@@ -72,10 +85,11 @@ fun MangaDetailsScreen(
 
         Column(
             modifier = modifier
-                .fillMaxSize(),
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState()),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Spacer(modifier = modifier.weight(2F))
+            Spacer(modifier = modifier.height(50.dp))
             AsyncImage(
                 model = mangaInfo?.data?.images?.webp?.large_image_url,
                 contentDescription = "Anime Cover",
@@ -107,6 +121,188 @@ fun MangaDetailsScreen(
             )
             Spacer(modifier = modifier.height(10.dp))
 
+            Row(
+                modifier = modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 10.dp)
+            ){
+                Icon(
+                    imageVector = Icons.Default.TrendingUp,
+                    contentDescription = "Popularity",
+                    tint = Color.Red
+                )
+                Spacer(modifier.width(5.dp))
+                Text(
+                    text = mangaInfo?.data?.popularity.toString(),
+                    fontFamily = poppinsFamily,
+                    fontSize = 18.sp
+                )
+
+                Spacer(modifier.weight(1F))
+
+                Icon(
+                    imageVector = Icons.Default.Star,
+                    contentDescription = "Ranking",
+                    tint = Color.Yellow
+                )
+                Spacer(modifier.width(5.dp))
+                Text(
+                    text = mangaInfo?.data?.rank.toString(),
+                    fontSize = 18.sp,
+                    fontFamily = poppinsFamily
+                )
+            }
+
+            Spacer(modifier.height(10.dp))
+
+            Row(
+                modifier = modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 10.dp)
+            ) {
+
+                Icon(
+                    imageVector = Icons.Default.LibraryBooks,
+                    contentDescription = "Chapters",
+                    tint = Color.Blue
+                )
+                Spacer(modifier.width(10.dp))
+                if (mangaInfo?.data?.volumes != null){
+                    Text(
+                        text = mangaInfo?.data?.volumes.toString(),
+                        fontFamily = poppinsFamily,
+                        fontSize = 18.sp
+                    )
+                }else{
+                    Text(
+                        text = "Ongoing",
+                        fontFamily = poppinsFamily,
+                        fontSize = 18.sp
+                    )
+                }
+
+                Spacer(modifier.weight(1F))
+
+                Icon(
+                    imageVector = Icons.Default.Article,
+                    contentDescription = "Chapters",
+                    tint = Color.Black
+                )
+                Spacer(modifier.width(10.dp))
+                Text(
+                    text = mangaInfo?.data?.chapters.toString(),
+                    fontFamily = poppinsFamily,
+                    fontSize = 18.sp
+                )
+            }
+
+            mangaInfo?.data?.published?.string.let {
+                Text(
+                    text = it.toString(),
+                    fontSize = 18.sp,
+                    fontFamily = poppinsFamily
+                )
+            }
+
+            Spacer(modifier.height(10.dp))
+
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 20.dp)
+            ){
+                Divider(
+                    modifier = modifier
+                        .weight(1f)
+                        .padding(end = 8.dp),
+                    color = Color.Black
+                )
+                Text(
+                    text = "Description",
+                    fontSize = 18.sp,
+                    fontFamily = poppinsFamily,
+                    color = Color.Black,
+                    fontWeight = FontWeight.Bold,
+                    textDecoration = TextDecoration.Underline
+                )
+                Divider(
+                    modifier = modifier
+                        .weight(1f)
+                        .padding(start = 8.dp),
+                    color = Color.Black
+                )
+            }
+
+            Spacer(modifier.height(10.dp))
+
+            mangaInfo?.data?.synopsis?.let {
+                Text(
+                    modifier = modifier
+                        .padding(horizontal = 10.dp),
+                    text = it,
+                    fontSize = 18.sp,
+                    fontFamily = poppinsFamily,
+                    textAlign = TextAlign.Left
+                )
+            }
+
+            Spacer(modifier.height(10.dp))
+
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 20.dp)
+            ){
+                Divider(
+                    modifier = modifier
+                        .weight(1f)
+                        .padding(end = 8.dp),
+                    color = Color.Black
+                )
+                Text(
+                    text = "Author",
+                    fontSize = 18.sp,
+                    fontFamily = poppinsFamily,
+                    color = Color.Black,
+                    fontWeight = FontWeight.Bold,
+                    textDecoration = TextDecoration.Underline
+                )
+                Divider(
+                    modifier = modifier
+                        .weight(1f)
+                        .padding(start = 8.dp),
+                    color = Color.Black
+                )
+            }
+
+            mangaInfo?.data?.authors?.forEach{author->
+                Column(
+                    modifier = modifier
+                        .fillMaxWidth()
+                        .clip(RoundedCornerShape(15.dp))
+                        .border(width = 1.dp, color = Color.Black, shape = RoundedCornerShape(15.dp))
+                        .padding(horizontal = 15.dp, vertical = 5.dp)
+                ) {
+                    Text(
+                        text = author.name,
+                        fontSize = 18.sp,
+                        fontFamily = poppinsFamily
+                    )
+                    Text(
+                        text = author.type,
+                        fontSize = 18.sp,
+                        fontFamily = poppinsFamily
+                    )
+                    Text(
+                        text = "more..",
+                        fontSize = 18.sp,
+                        fontFamily = poppinsFamily
+                    )
+                }
+            }
+
             Spacer(modifier.height(10.dp))
             Button(
                 modifier = modifier
@@ -129,14 +325,22 @@ fun MangaDetailsScreen(
                     mangaViewModel.addMangaToFavorite(favManga)
                 }
             ) {
-                Text(
-                    text = "Add to Favorites",
-                    fontSize = 18.sp,
-                    fontFamily = poppinsFamily
-                )
+                Row {
+                    Icon(
+                        imageVector = Icons.Default.Favorite,
+                        contentDescription = "Favorite!",
+                        tint = Color.Red
+                    )
+                    Spacer(modifier = modifier.width(10.dp))
+                    Text(
+                        text = "Add to Favorites",
+                        fontSize = 18.sp,
+                        fontFamily = poppinsFamily
+                    )
+                }
             }
 
-            Spacer(modifier = modifier.weight(8F))
+            Spacer(modifier = modifier.height(50.dp))
         }
     }
 }
